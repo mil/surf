6643ed21ea6883ffdc84a7b3faf8e19de714245a
Miles Alan <m@milesalan.com>
2019-09-15 11:12:47 -0500

    Search !sp by default if given url entry with a space

--- surf.c
+++ surf.c
@@ -632,8 +632,10 @@ loaduri(Client *c, const Arg *a)
 	if (g_strcmp0(uri, "") == 0)
 		return;
 
-  if (g_str_has_prefix(uri, " ") || g_regex_match_simple("\\s*!\\S+\\s*", uri, 0, 0)) {
+  if (g_regex_match_simple("\\s*!\\S+\\s*", uri, 0, 0)) {
 		url = g_strdup_printf("%s%s", searchengine, uri);
+  } else if (g_regex_match_simple(".*\\s.*", uri, 0, 0)) {
+		url = g_strdup_printf("%s!sp %s", searchengine, uri);
   } else if (g_str_has_prefix(uri, "http://")  ||
 	    g_str_has_prefix(uri, "https://") ||
 	    g_str_has_prefix(uri, "file://")  ||
